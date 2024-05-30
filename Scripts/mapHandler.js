08b45cf202a33bf4f888e50beff180b167208eb9
Strelia Illia <seylvin2005@gmail.com>
2024-05-30 19:28:54 +0300

    Added leaflet map
    - Added support of map
    - Changed index.html for map
    - Added handler for map

---
+++ Scripts/mapHandler.js
@@ -0,0 +1,11 @@
+const map = L.map('map').setView([54.526, 15.2551], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+export default function scrollToCity(x, y, zoom) {
+  map.flyTo([x, y], zoom);
+}
--- Scripts/weather.js
+++ Scripts/weather.js
@@ -1,3 +1,5 @@
+import scrollToCity from './mapHandler.js';
+
 const city = document.querySelector('.city-name');
 const currentTemp = document.querySelector('.temp');
 const lastUpdateDate = document.querySelector('.date-time');
@@ -23,16 +25,20 @@ export default async function fetchWeather() {
   const url = `http://api.weatherapi.com/v1/forecast.json?key=45d690a2e9744e09879101551242905&q=${city}`;
   const response = await fetch(url, { method: 'GET' });
 
-  parseWeatherObj(await response.json());
+  parseWeatherObj(await response.json()).then(coord => {
+    const { lat, lon } = coord;
+    scrollToCity(lat, lon, 15);
+  });
 }
 
-const parseWeatherObj = function (weatherObj) {
+const parseWeatherObj = async function (weatherObj) {
   const { current, forecast, location } = weatherObj;
   lastUpdateDate.textContent = formaterDate.format(
     new Date(current.last_updated)
   );
-
   city.textContent = location.name;
   currentTemp.textContent = formaterTemp.format(current.temp_c);
   weatherIcon.setAttribute('src', current.condition.icon);
+  console.log(location);
+  return { lat: location.lat, lon: location.lon };
 };
