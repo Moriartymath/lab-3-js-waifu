d6688235600bd9bf8e9c33d4423f2964d11968e6
Moriarty <116695498+Moriartymath@users.noreply.github.com>
2024-06-10 15:34:19 +0300

    Added styling to display recent places with information

--- Scripts/weatherInfo.ts
+++ Scripts/weatherInfo.ts
@@ -10,13 +10,30 @@ class WeatherInfo {
   private static diplayRecentPlaces(): void {
     const allPlaces = StoragePlaces.getAllPlaces();
 
-    allPlaces.slice(-4).forEach(placeName => {
+    allPlaces.slice().forEach(placeName => {
       const htmlStr = `
-      <div class="place ${placeName.toLowerCase()}--place"><a>${placeName}</a></div>
+      <div class="place ${placeName.toLowerCase()}--place">
+      <div class="location--date">
+      <a class="city--name">${placeName}</a>
+      <p class="city--time">${new Date().getHours()}</p>
+      </div>
+      <div class="city--conditions"> 
+      <span class="city--current--temp">27C</span>
+      <div class="temp-low--high"> 
+      <span>H:23</span>
+      <span>L:10</span>
+      </div>
+      </div>
+      </div>
       `;
 
       recentPlaces?.insertAdjacentHTML('beforeend', htmlStr);
     });
+  }
+
+  private static setUpdateTime(cityTag: string): void {
+    const cityElement = document.querySelector(`.${cityTag}`);
+    setInterval(() => {}, 1000 * 60);
   }
 }
 
